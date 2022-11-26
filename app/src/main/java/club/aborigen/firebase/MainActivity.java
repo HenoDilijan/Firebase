c704bd731e4b087857097dff330ad5eaef4e7578
Henrik Gardishyan <henrik.gardishyan@gmail.com>
2022-11-26 23:20:56 +0400

    Realtime database added

--- app/src/main/java/club/aborigen/firebase/MainActivity.java
+++ app/src/main/java/club/aborigen/firebase/MainActivity.java
@@ -8,9 +8,17 @@ import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+    DatabaseReference database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,24 @@ public class MainActivity extends AppCompatActivity {
         FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task ->
                 Log.i("UWC", "FCM token: " + task.getResult())
         );
+
+        database = FirebaseDatabase.getInstance().getReference("Shake");
+
+        Log.i("UWC", "Reading from " + database.getKey());
+
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.i("UWC", "Value is: " + dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.e("UWC", "Failed to read value.", error.toException());
+            }
+        };
+        database.addValueEventListener(postListener);
+
+        database.setValue("Burger");
     }
 }
